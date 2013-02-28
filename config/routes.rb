bc7eae62c9d31dca0b4e59ec0d2ab5b739c9e728
Chris <laotzu02@hotmail.com>
2013-02-28 01:36:37 -0500

    completed signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ SampleApp::Application.routes.draw do
 
   match '/signup',  to: 'users#new'
 
-  match '/signup', to: 'users#new'
   match '/home',    to: 'static_pages#home'  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
