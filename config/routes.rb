66455c70b2ed3d3421bfb0280f5cefca344a98f6
Chris <laotzu02@hotmail.com>
2013-02-28 02:48:35 -0500

    initialized signin process

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/home',    to: 'static_pages#home'  
   match '/help',    to: 'static_pages#help'
